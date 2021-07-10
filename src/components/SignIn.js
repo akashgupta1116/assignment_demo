890cc182e4e5fccc8b1bc73801a7b234b8f1e616
akashgupta1116 <53077427+akashgupta1116@users.noreply.github.com>
2021-07-10 17:13:11 +0530

    Add styles to Global Stylesheet + logo added

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
-import "./signin.css";
 import { StyledButton, StyledInput } from "./CustomStyledComponents";
+
+const logoStyle = {
+  marginTop: "20%",
+  width: "35vh"
+};
 
 const SignIn = () => {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <div className="signin-container">
-      <p className="welcome-msg bold-text">WELCOME TO SITE CONTROL</p>
+    <div className="container">
+      <div style={logoStyle}>
+        <img
+          style={{ width: "100%" }}
+          src={require("../../assets/Logo 2 iteration.png")}
+          alt="logo"
+        />
+      </div>
+      <p className="headline bold-text">WELCOME TO SITE CONTROL</p>
       <StyledInput
         type="text"
         name="userName"
@@ -26,10 +37,15 @@ const SignIn = () => {
         onChange={(e) => setPassword(e.target.value)}
       />
       <StyledButton className="signin-btn">Sign In</StyledButton>
-      <a className="login-links">Forgot your details?</a>
-      <a className="login-links bold-text">Product Of Kreya Infratech</a>
+      <a className="links">Forgot your details?</a>
+      <a className="links bold-text">Product Of Kreya Infratech</a>
     </div>
   );
 };
 
 export default SignIn;
+
+/* TODO:
+ * Inline Stylining to removed
+ * Logo need to changed
+ */
